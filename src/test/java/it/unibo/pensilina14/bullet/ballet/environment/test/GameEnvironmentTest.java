6b7da900d636e848805e8f20faf77f68232d3f3e
daze <leon.baiocchi@studio.unibo.it>
2021-09-06 16:19:47 +0200

    added res folder, setup working branch

---
+++ src/test/java/it/unibo/pensilina14/bullet/ballet/environment/test/GameEnvironmentTest.java
@@ -0,0 +1,14 @@
+package it.unibo.pensilina14.bullet.ballet.environment.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameEnvironmentTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
