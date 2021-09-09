614a246a1a43f3c0765b2fb27808f276cf26dd6d
daze <leon.baiocchi@studio.unibo.it>
2021-09-09 18:06:42 +0200

    add another test for the other constructor

--- src/test/java/it/unibo/pensilina14/bullet/ballet/environment/test/GameEnvironmentTest.java
+++ src/test/java/it/unibo/pensilina14/bullet/ballet/environment/test/GameEnvironmentTest.java
@@ -1,7 +1,6 @@
 package it.unibo.pensilina14.bullet.ballet.environment.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
 
 import it.unibo.pensilina14.bullet.ballet.environment.Dimension2Dimpl;
 import it.unibo.pensilina14.bullet.ballet.environment.Environment;
@@ -16,13 +15,33 @@ import org.junit.Test;
 
 public class GameEnvironmentTest {
   private static final double EARTH_GRAVITY = 9.81;
+  private static final double MOON_GRAVITY = 6.673;
   private static final int DEFAULT_DIM = 500;
+  private static final double DELTA = 0.01;
 
   @Test
   public void testGameEnvironment() {
     final Environment gameEnv = new GameEnvironment();
     
-    assertEquals(gameEnv.getGravity(), EARTH_GRAVITY, 0.01);
+    assertEquals(gameEnv.getGravity(), EARTH_GRAVITY, DELTA);
+    assertEquals(gameEnv.getDimension(), new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM));
+    
+    final Map<ImmutablePosition2D, Optional<PhysicalObject>> expectedMap = new HashMap<>();
+    for (int x = 0; x < gameEnv.getDimension().getWidth(); x++) {
+      for (int y = 0; y < gameEnv.getDimension().getHeight(); y++) {
+        expectedMap.put(new ImmutablePosition2Dimpl(x, y), Optional.empty());
+      }
+    }
+    
+    assertEquals(gameEnv.getMap(), expectedMap);
+  }
+  
+  @Test
+  public void testGameEnvironmentWithArgs() {
+    final Environment gameEnv = new GameEnvironment(MOON_GRAVITY, 
+        new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM));
+    
+    assertEquals(gameEnv.getGravity(), MOON_GRAVITY, DELTA);
     assertEquals(gameEnv.getDimension(), new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM));
     
     final Map<ImmutablePosition2D, Optional<PhysicalObject>> expectedMap = new HashMap<>();
