fd3c981f69b53281bc4001b626b88ed36dc81d0c
Luca <luca.rengo@studio.unibo.it>
2021-09-10 18:28:36 +0200

    Added method isAlive()

--- src/main/java/it/unibo/pensilina14/bullet/ballet/characters/Characters.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/characters/Characters.java
@@ -2,9 +2,24 @@ package it.unibo.pensilina14.bullet.ballet.characters;
 
 public interface Characters {
 
+    /**
+     *
+     * @return health of the character.
+     */
+
     float getHealth();
+
+    /**
+     *
+     * @return mana of the character.
+     */
 
     float getMana();
 
-    
+    /**
+     *
+     * @return whether character is alive or not.
+     */
+
+    boolean isAlive(); // isDead
 }
