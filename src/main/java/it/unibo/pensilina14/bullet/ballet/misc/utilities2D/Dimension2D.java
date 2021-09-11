3df1470cf6539d6a57339597570bc66c092c18ef
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-09-11 23:11:17 +0200

    add documentation to dimension2D

--- src/main/java/it/unibo/pensilina14/bullet/ballet/misc/utilities2D/Dimension2D.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/misc/utilities2D/Dimension2D.java
@@ -3,11 +3,22 @@ package it.unibo.pensilina14.bullet.ballet.misc.utilities2D;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 public interface Dimension2D {
-    
+  /**
+   * 
+   * @return object's dimensions
+   */
   ImmutablePair<Integer, Integer> getSize();
   
+  /**
+   * 
+   * @return object's height
+   */
   int getHeight();
   
+  /**
+   * 
+   * @return object's width
+   */
   int getWidth();
     
 }
