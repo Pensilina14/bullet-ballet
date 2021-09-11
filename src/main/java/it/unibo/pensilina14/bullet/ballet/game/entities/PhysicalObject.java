98baa83566a8491c0f8024648ed53ba8523bda40
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-09-11 11:41:51 +0200

    add getEnvironment method to PhysicalObject interface

--- src/main/java/it/unibo/pensilina14/bullet/ballet/game/entities/PhysicalObject.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/game/entities/PhysicalObject.java
@@ -1,5 +1,6 @@
 package it.unibo.pensilina14.bullet.ballet.game.entities;
 
+import it.unibo.pensilina14.bullet.ballet.environment.Environment;
 import it.unibo.pensilina14.bullet.ballet.misc.utilities2D.Dimension2D;
 import it.unibo.pensilina14.bullet.ballet.misc.utilities2D.MutablePosition2D;
 
@@ -23,4 +24,9 @@ public interface PhysicalObject {
  * 
  */
   Dimension2D getDimension();
+  
+  /*
+   * 
+  */
+  Environment getGameEnvironment();
 }
