c83e39606739810f433b7e65434f61ddf0b5b17e
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-09-11 22:54:16 +0200

    add pickUpItem

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/weapon/PickupItem.java
@@ -0,0 +1,24 @@
+package it.unibo.pensilina14.bullet.ballet.weapon;
+
+import it.unibo.pensilina14.bullet.ballet.environment.Environment;
+import it.unibo.pensilina14.bullet.ballet.game.entities.AbstractDynamicComponent;
+import it.unibo.pensilina14.bullet.ballet.misc.utilities2D.Dimension2D;
+import it.unibo.pensilina14.bullet.ballet.misc.utilities2D.MutablePosition2D;
+
+public class PickupItem extends AbstractDynamicComponent implements Item{
+    
+    private final Effect effect;
+
+    public PickupItem(Dimension2D dimension, MutablePosition2D position
+            , Environment gameEnvironment, double mass,
+            Effect effect) {
+        super(dimension, position, gameEnvironment, mass);
+        this.effect = effect;
+    }
+
+    @Override
+    public Effect getEffect() {
+        return this.effect;
+    }
+    
+}
