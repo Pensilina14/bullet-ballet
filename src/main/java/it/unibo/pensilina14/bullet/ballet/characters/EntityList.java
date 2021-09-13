4b1e0120f8e9fa49ea253699c26dc5ecbef95778
Luca <luca.rengo@studio.unibo.it>
2021-09-13 18:35:14 +0200

    Added EntityList enum file.

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/characters/EntityList.java
@@ -0,0 +1,18 @@
+package it.unibo.pensilina14.bullet.ballet.characters;
+
+public enum EntityList {
+
+    CHARACTER;
+
+    enum Player{
+        PLAYER1,
+        PLAYER2,
+        PLAYER3
+    }
+
+    enum Enemy{
+        ENEMY1,
+        ENEMY2,
+        ENEMY3
+    }
+}
