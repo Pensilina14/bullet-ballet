02d8a5b79648d256437c8564b9ef3e68503d7cc5
Luca <luca.rengo@studio.unibo.it>
2021-09-13 18:36:41 +0200

    Added TODO comments.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/characters/EntityList.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/characters/EntityList.java
@@ -5,13 +5,13 @@ public enum EntityList {
     CHARACTER;
 
     enum Player{
-        PLAYER1,
+        PLAYER1, // TODO: mettere i nomi dei players al posto di PLAYER
         PLAYER2,
         PLAYER3
     }
 
     enum Enemy{
-        ENEMY1,
+        ENEMY1, // TODO: mettere i nomi degli enemies al posto di ENEMY
         ENEMY2,
         ENEMY3
     }
