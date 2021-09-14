e6eeedc4fae0e854c4ca7396d3ba73632bb28fe6
daze <leon.baiocchi@studio.unibo.it>
2021-09-14 15:11:54 +0200

    Add Javadoc

--- src/main/java/it/unibo/pensilina14/bullet/ballet/game/effects/AbstractBonusEffect.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/game/effects/AbstractBonusEffect.java
@@ -1,17 +1,41 @@
 package it.unibo.pensilina14.bullet.ballet.game.effects;
 
+/**
+ * This abstract class serves as a template
+ * for all the possible implementations of {@link Effect} 
+ * interface that include POSITIVE consequences after 
+ * the method {@link Effect#applyEffect()}.
+ */
 public abstract class AbstractBonusEffect implements Effect {
-
+	/**
+	 * Every effect has a target {@link Character}, referred to by this field.
+	 */
 	private final Character target;
 
+	/**
+	 * Constructor: asks for a target, which in this case is a {@link Character} 
+	 * object and sets it as field {@link #target} value.
+	 * 
+	 * @param target
+	 */
 	public AbstractBonusEffect(final Character target) {
 		this.target = target;
 	}
 
+	/**
+	 * Returns effect's target.
+	 * 
+	 * @return {@link #target} 
+	 */
 	public final Character getTarget() {
 		return this.target;
 	}
 
+	/**
+	 * This method must be implemented in inheriting class.
+	 * 
+	 * {@inheritDoc}
+	 */
 	@Override
 	public abstract void applyEffect();
 }
--- src/main/java/it/unibo/pensilina14/bullet/ballet/game/effects/AbstractMalusEffect.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/game/effects/AbstractMalusEffect.java
@@ -1,17 +1,41 @@
 package it.unibo.pensilina14.bullet.ballet.game.effects;
 
+/**
+ * This abstract class serves as a template
+ * for all the possible implementations of {@link Effect} 
+ * interface that include NEGATIVE consequences after 
+ * the method {@link Effect#applyEffect()}.
+ */
 public abstract class AbstractMalusEffect implements Effect {
-
+	/**
+	 * Every effect has a target {@link Character}, referred to by this field.
+	 */
 	private final Character target;
 
+	/**
+	 * Constructor: asks for a target, which in this case is a {@link Character} 
+	 * object and sets it as field {@link #target} value.
+	 * 
+	 * @param target
+	 */
 	public AbstractMalusEffect(final Character target) {
 		this.target = target;
 	}
 
+	/**
+	 * Returns effect's target.
+	 * 
+	 * @return {@link #target} 
+	 */
 	public final Character getTarget() {
 		return this.target;
 	}
 	
+	/**
+	 * This method must be implemented in inheriting class.
+	 * 
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void applyEffect() {
 		// TODO Auto-generated method stub
