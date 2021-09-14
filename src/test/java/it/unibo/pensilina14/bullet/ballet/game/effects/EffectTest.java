93ce707a4e1210482cbf3f9a44d6b2901e040a38
daze <leon.baiocchi@studio.unibo.it>
2021-09-14 19:59:01 +0200

    set up junit test case for Effect interface usage

---
+++ src/test/java/it/unibo/pensilina14/bullet/ballet/game/effects/EffectTest.java
@@ -0,0 +1,27 @@
+package it.unibo.pensilina14.bullet.ballet.game.effects;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+import it.unibo.pensilina14.bullet.ballet.characters.Enemy;
+import it.unibo.pensilina14.bullet.ballet.environment.Environment;
+import it.unibo.pensilina14.bullet.ballet.environment.GameEnvironment;
+import it.unibo.pensilina14.bullet.ballet.misc.utilities2D.Dimension2Dimpl;
+import it.unibo.pensilina14.bullet.ballet.misc.utilities2D.MutablePosition2Dimpl;
+import it.unibo.pensilina14.bullet.ballet.weapon.PickupItem;
+
+public class EffectTest {
+
+//	@Test
+//	public void testApplyEffect() {
+//		final Environment env = new GameEnvironment();
+//		final Enemy enemy = new Enemy("testEnemy00", 100.0, Optional.ofNullable(150.0), 
+//				0, new Dimension2Dimpl(25, 5), null, , 85.0);
+//		final PickupItem item = new PickupItem(new Dimension2Dimpl(25, 5),
+//				new MutablePosition2Dimpl(150, 50), env, 3.5, t -> t.);
+//	}
+
+}
