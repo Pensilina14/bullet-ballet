479be5248e2933c40fd10f78185392e059ff3853
daze <leon.baiocchi@studio.unibo.it>
2021-09-14 13:05:43 +0200

    refactoring and Javadoc additions/fixes

--- src/main/java/it/unibo/pensilina14/bullet/ballet/environment/GameEnvironment.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/environment/GameEnvironment.java
@@ -18,101 +18,99 @@ import java.util.stream.Collectors;
  */
 public class GameEnvironment implements Environment {
 
-  private final double gravity;
-  private final Dimension2D gameMapDim;
-  private final Map<ImmutablePosition2D, Optional<PhysicalObject>> gameMap;
-  public static final int DEFAULT_DIM = 500;
+	private final double gravity;
+	private final Dimension2D gameMapDim;
+	private final Map<ImmutablePosition2D, Optional<PhysicalObject>> gameMap;
+	/**
+	 * DEFAULT_DIM is the default size used to create a new
+	 * {@link Dimension2D}.
+	 */
+	public static final int DEFAULT_DIM = 500;
+	/**
+	 * EARTH_GRAVITY represents earth's {@link gravity} value.
+	 */
+	public static final double EARTH_GRAVITY = 9.81;
 
-  /*
-   * Constructor method in which {@link gravity} 
-   * is set at earth one's value and {@link gameMap}'s 
-   * coordinates are automatically set.
-   * {@link gameMapSize} set to 500 by default.
-   */
-  public GameEnvironment() {
-    this.gravity = 9.81;
-    this.gameMapDim = new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM);
-    //generate virtual map's coordinates as gameMap keys
-    this.gameMap = new HashMap<>();
-    this.initializeMap(this.gameMap, this.gameMapDim);
-  }
+	/**
+	 * Constructor method in which {@link gravity} is set at earth one's value and
+	 * {@link gameMap}'s coordinates are automatically set. {@link gameMapSize} set
+	 * to 500 by default.
+	 */
+	public GameEnvironment() {
+		this.gravity = EARTH_GRAVITY;
+		this.gameMapDim = new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM);
+		// generate virtual map's coordinates as gameMap keys
+		this.gameMap = new HashMap<>();
+		this.initializeMap(this.gameMap, this.gameMapDim);
+	}
 
-  /*
-   * Constructor method in which {@link gravity}
-   * is set at @param gravity and {@link Dimension2D} is 
-   * set at @param size 
-   * {@link gameMap} is initialized
-   */
-  public GameEnvironment(final double gravity, final Dimension2D size) {
-    this.gravity = gravity;
-    this.gameMapDim = size;
-    this.gameMap = new HashMap<>();
-    this.initializeMap(this.gameMap, this.gameMapDim);
-  }
+	/*
+	 * Constructor method in which {@link gravity} is set at @param gravity and
+	 * {@link Dimension2D} is set at @param size {@link gameMap} is initialized
+	 */
+	public GameEnvironment(final double gravity, final Dimension2D size) {
+		this.gravity = gravity;
+		this.gameMapDim = size;
+		this.gameMap = new HashMap<>();
+		this.initializeMap(this.gameMap, this.gameMapDim);
+	}
 
-  /*
-   * Generates virtual map's coordinates as gameMap keys.
-   */
-  private void initializeMap(final Map<ImmutablePosition2D, Optional<PhysicalObject>> map, 
-      final Dimension2D mapDim) {
-    for (int x = 0; x < mapDim.getWidth(); x++) { 
-      for (int y = 0; y < mapDim.getHeight(); y++) {
-        map.put(new ImmutablePosition2Dimpl(x, y), Optional.empty());
-      }
-    }
-  }
+	/*
+	 * Generates virtual map's coordinates as gameMap keys.
+	 */
+	private void initializeMap(final Map<ImmutablePosition2D, Optional<PhysicalObject>> map, final Dimension2D mapDim) {
+		for (int x = 0; x < mapDim.getWidth(); x++) {
+			for (int y = 0; y < mapDim.getHeight(); y++) {
+				map.put(new ImmutablePosition2Dimpl(x, y), Optional.empty());
+			}
+		}
+	}
 
-  @Override
-  public final double getGravity() {
-    return this.gravity;
-  }
+	@Override
+	public final double getGravity() {
+		return this.gravity;
+	}
 
-  @Override
-  public final Dimension2D getDimension() {
-    return this.gameMapDim;
-  }
+	@Override
+	public final Dimension2D getDimension() {
+		return this.gameMapDim;
+	}
 
-  @Override
-  public final Map<ImmutablePosition2D, Optional<PhysicalObject>> getMap() {
-    return Map.copyOf(this.gameMap);
-  }
+	@Override
+	public final Map<ImmutablePosition2D, Optional<PhysicalObject>> getMap() {
+		return Map.copyOf(this.gameMap);
+	}
 
-  @Override
-  public final boolean addObjToMap(final PhysicalObject obj, final ImmutablePosition2D pos) {
-    if (this.gameMap.containsKey(pos) && this.gameMap.get(pos).equals(Optional.empty())) {
-      this.gameMap.put(pos, Optional.ofNullable(obj));
-      return true;
-    } else {
-      return false;
-    }
-  }
+	@Override
+	public final boolean addObjToMap(final PhysicalObject obj, final ImmutablePosition2D head) {
+		if (this.gameMap.containsKey(head) && this.gameMap.get(head).equals(Optional.empty())) {
+			this.gameMap.put(head, Optional.ofNullable(obj));
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  @Override
-  public final void deleteObjByPosition(final ImmutablePosition2D position) {
-    this.gameMap.remove(position);
-  }
+	@Override
+	public final void deleteObjByPosition(final ImmutablePosition2D position) {
+		this.gameMap.remove(position);
+	}
 
-  @Override
-  public final Optional<ImmutablePosition2D> findObjInMap(final PhysicalObject obj) {
-    for (final Map.Entry<ImmutablePosition2D, Optional<PhysicalObject>> 
-            entry : this.gameMap.entrySet()) {
-      if (entry.getValue().isPresent() && entry.getValue().get().equals(obj)) {
-        return Optional.of(
-          new ImmutablePosition2Dimpl(obj.getPosition().getX(), 
-            obj.getPosition().getY()
-        ));
-      }
-    } 
-    return Optional.empty();
-  }
+	@Override
+	public final Optional<ImmutablePosition2D> findObjInMap(final PhysicalObject obj) {
+		for (final Map.Entry<ImmutablePosition2D, Optional<PhysicalObject>> entry : this.gameMap.entrySet()) {
+			if (entry.getValue().isPresent() && entry.getValue().get().equals(obj)) {
+				return Optional.of(new ImmutablePosition2Dimpl(obj.getPosition().getX(), obj.getPosition().getY()));
+			}
+		}
+		return Optional.empty();
+	}
 
-
-  @Override
-  public final Optional<List<PhysicalObject>> getObjListInMap() {
-    final List<PhysicalObject> objList = new ArrayList<>();
-    this.gameMap.entrySet().stream()
-        .filter(e -> e.getValue().equals(Optional.empty()))
-        .collect(Collectors.toList());
-    return Optional.ofNullable(objList);
-  } 
+	@Override
+	public final Optional<List<PhysicalObject>> getObjListInMap() {
+		final List<PhysicalObject> objList = new ArrayList<>();
+		this.gameMap.entrySet().stream().filter(e -> e.getValue().equals(Optional.empty()))
+				.collect(Collectors.toList());
+		return Optional.ofNullable(objList);
+	}
 }
