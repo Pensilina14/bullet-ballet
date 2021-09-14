5686f1364816b26e83ff56c5b4de263a30482392
daze <leon.baiocchi@studio.unibo.it>
2021-09-14 13:10:19 +0200

    do implement the other abstract class (AbstractMalusEffect)

--- src/main/java/it/unibo/pensilina14/bullet/ballet/game/effects/AbstractMalusEffect.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/game/effects/AbstractMalusEffect.java
@@ -2,6 +2,16 @@ package it.unibo.pensilina14.bullet.ballet.game.effects;
 
 public abstract class AbstractMalusEffect implements Effect {
 
+	private final Character target;
+
+	public AbstractMalusEffect(final Character target) {
+		this.target = target;
+	}
+
+	public final Character getTarget() {
+		return this.target;
+	}
+	
 	@Override
 	public void applyEffect() {
 		// TODO Auto-generated method stub
