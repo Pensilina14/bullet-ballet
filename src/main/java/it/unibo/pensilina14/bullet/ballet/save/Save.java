3a47fd040b1cb481a5db11ec6fa77836670a2284
Luca <luca.rengo@studio.unibo.it>
2021-09-15 21:24:56 +0200

    Created Save and SaveTest classes.

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/save/Save.java
@@ -0,0 +1,7 @@
+package it.unibo.pensilina14.bullet.ballet.save;
+
+import java.io.Serializable;
+
+public class Save implements Serializable {
+
+}
---
+++ src/test/java/it/unibo/pensilina14/bullet/ballet/save/SaveTest.java
@@ -0,0 +1,11 @@
+package it.unibo.pensilina14.bullet.ballet.save;
+
+import org.junit.Test;
+
+public class SaveTest {
+
+    @Test
+    public void testSaveData(){
+
+    }
+}
