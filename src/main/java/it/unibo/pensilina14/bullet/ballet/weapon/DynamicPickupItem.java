623a3a93e9788dc7898269874b403c75fc559f6c
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-09-17 09:03:47 +0200

    add hashcode and equals to Item impl

--- src/main/java/it/unibo/pensilina14/bullet/ballet/weapon/DynamicPickupItem.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/weapon/DynamicPickupItem.java
@@ -1,7 +1,5 @@
 package it.unibo.pensilina14.bullet.ballet.weapon;
 
-import java.util.stream.IntStream;
-
 import it.unibo.pensilina14.bullet.ballet.environment.Environment;
 import it.unibo.pensilina14.bullet.ballet.game.effects.Effect;
 import it.unibo.pensilina14.bullet.ballet.game.entities.AbstractDynamicComponent;
@@ -37,6 +35,34 @@ public class DynamicPickupItem extends AbstractDynamicComponent implements Item{
     public void yAxisMovement() {
         while (this.moveUP(1)) {};
         while (this.moveDOWN(1)) {};
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((effect == null) ? 0 : effect.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DynamicPickupItem other = (DynamicPickupItem) obj;
+        if (effect == null) {
+            if (other.effect != null)
+                return false;
+        } else if (!effect.equals(other.effect))
+            return false;
+        if (id != other.id)
+            return false;
+        return true;
     }
     
 }
--- src/main/java/it/unibo/pensilina14/bullet/ballet/weapon/StaticPickUpItem.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/weapon/StaticPickUpItem.java
@@ -28,8 +28,33 @@ public class StaticPickUpItem extends AbstractDynamicComponent implements Item{
     public Effect getEffect() {
         return this.effect;
     }
-    
-    
-    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((effect == null) ? 0 : effect.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StaticPickUpItem other = (StaticPickUpItem) obj;
+        if (effect == null) {
+            if (other.effect != null)
+                return false;
+        } else if (!effect.equals(other.effect))
+            return false;
+        if (id != other.id)
+            return false;
+        return true;
+    }
     
 }
