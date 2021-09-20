90fa896f601883499467822939030855c413d6dd
daze <leon.baiocchi@studio.unibo.it>
2021-09-20 19:02:00 +0200

    modified but actually modified: it isn't my part

--- src/main/java/it/unibo/pensilina14/bullet/ballet/model/entities/PhysicalObject.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/entities/PhysicalObject.java
@@ -9,24 +9,25 @@ import it.unibo.pensilina14.bullet.ballet.model.environment.Environment;
  *
  */
 public interface PhysicalObject {
- /*
- * @return position in coordinates.
- */
-  MutablePosition2D getPosition();
+	/*
+	 * @return position in coordinates.
+	 */
+	MutablePosition2D getPosition();
 
- /*
- * @return true if this PhysicalObject is colliding with {@param other},
- * @return false otherwise.
- */
-  Boolean isCollidingWith(PhysicalObject other);
-  
- /*
- * 
- */
-  Dimension2D getDimension();
-  
-  /*
-   * 
-  */
-  Environment getGameEnvironment();
+	/*
+	 * @return true if this PhysicalObject is colliding with {@param other},
+	 * 
+	 * @return false otherwise.
+	 */
+	Boolean isCollidingWith(PhysicalObject other);
+
+	/*
+	* 
+	*/
+	Dimension2D getDimension();
+
+	/*
+	 * 
+	*/
+	Environment getGameEnvironment();
 }
