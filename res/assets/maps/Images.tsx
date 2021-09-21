f2bb0e2a7156e267f5f666e1f09e5c4b53d0ed61
Luca <luca.rengo@studio.unibo.it>
2021-09-21 19:37:48 +0200

    Add maps assets file

---
+++ res/assets/maps/Images.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Images" tilewidth="2560" tileheight="1600" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="700" height="400" source="Backgrounds/cave_background.png"/>
+ </tile>
+ <tile id="1">
+  <image width="1920" height="1080" source="Backgrounds/cave_background2.jpg"/>
+ </tile>
+ <tile id="2">
+  <image width="1024" height="614" source="Backgrounds/cave_background3.jpg"/>
+ </tile>
+ <tile id="3">
+  <image width="1280" height="640" source="Backgrounds/desert_background.jpg"/>
+ </tile>
+ <tile id="4">
+  <image width="1280" height="640" source="Backgrounds/desert_background2.jpg"/>
+ </tile>
+ <tile id="5">
+  <image width="1700" height="745" source="Backgrounds/futuristic_background.jpg"/>
+ </tile>
+ <tile id="6">
+  <image width="2560" height="1600" source="Backgrounds/jungle_background.jpg"/>
+ </tile>
+ <tile id="7">
+  <image width="900" height="473" source="Backgrounds/jungle_background.png"/>
+ </tile>
+ <tile id="8">
+  <image width="1280" height="720" source="Backgrounds/jungle_background2.jpg"/>
+ </tile>
+ <tile id="9">
+  <image width="1300" height="742" source="Backgrounds/lava_background.png"/>
+ </tile>
+ <tile id="10">
+  <image width="1920" height="817" source="Backgrounds/spooky_background.jpg"/>
+ </tile>
+</tileset>
---
+++ res/assets/maps/tmw_desert_spacing.tsx
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tmw_desert_spacing" tilewidth="32" tileheight="32" spacing="1" tilecount="48" columns="8">
+ <image source="tmw_desert_spacing.png" width="265" height="199"/>
+ <wangsets>
+  <wangset name="Brick" type="corner" tile="9">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="8" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="10" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="19" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="27" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="28" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+  <wangset name="Stone" type="corner" tile="33">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="24" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="25" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="26" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="32" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="33" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="34" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="35" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="36" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="40" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="41" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="42" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="43" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="44" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
