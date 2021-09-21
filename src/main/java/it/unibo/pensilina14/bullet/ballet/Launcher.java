d6eea34786d7f2b2b510a7e47b1f1e01a4aefa9b
Luca <luca.rengo@studio.unibo.it>
2021-09-21 19:35:40 +0200

    Modify main to test game view.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/Launcher.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/Launcher.java
@@ -1,13 +1,24 @@
 package it.unibo.pensilina14.bullet.ballet;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import it.unibo.pensilina14.bullet.ballet.core.GameEngine;
+import it.unibo.pensilina14.bullet.ballet.graphics.map.Map;
 
 public final class Launcher {
 
 	public static void main(String[] args) {
-		final GameEngine game = new GameEngine();
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Bullet Ballet";
+		cfg.useGL30 = false;
+		cfg.width = 1280;
+		cfg.height = 720;
+
+		new LwjglApplication(new Map(), cfg);
+
+		/*final GameEngine game = new GameEngine();
 		game.setup();
-		game.mainLoop();
+		game.mainLoop();*/
 	}
 
 }
