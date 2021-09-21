f33ebac92b96093eeb6d674b149e5734d596d39a
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-09-21 18:28:50 +0200

    add settings fxml with controller

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/SettingsController.java
@@ -0,0 +1,41 @@
+package it.unibo.pensilina14.bullet.ballet.menu.controller;
+
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+
+public class SettingsController {
+
+    @FXML
+    private Slider audio;
+    @FXML
+    private ComboBox<String> resolution;
+    @FXML
+    private ComboBox<String> difficulty;
+
+    @FXML
+    void goBackOnMouseClick(final MouseEvent event) throws IOException {
+        final PageLoader loader = new PageLoader();
+        loader.goToSelectedPage("homepage.fxml", event);
+    }
+
+    @FXML
+    void showDifficultiesOnMouseClick(final MouseEvent event) {
+        final ObservableList<String> difficulties = FXCollections.observableArrayList("dumb", "easy", 
+                "medium", "hard", "godlike");
+        this.difficulty.setItems(difficulties);
+    }
+
+    @FXML
+    void showResolutionsOnMouseClicked(final MouseEvent event) {
+        final ObservableList<String> resolutions = FXCollections.observableArrayList("1920x1080",
+                "1024x768");
+        this.resolution.setItems(resolutions);
+    }
+    
+}
