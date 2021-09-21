40677453383d1a46f4ee3f73e7c55d820d2b1f2c
daze <leon.baiocchi@studio.unibo.it>
2021-09-21 22:03:19 +0200

    add poison effect in factory

--- src/main/java/it/unibo/pensilina14/bullet/ballet/model/effects/EffectFactory.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/effects/EffectFactory.java
@@ -17,12 +17,21 @@ public interface EffectFactory {
 	 */
 	Effect createHealEffect(Characters target, double healthIncreaseFactor);
 	/**
-	 * Creates an effect capable of poisoning a given {@link Characters}.
+	 * Creates an effect capable of damaging a given {@link Characters}.
 	 * 
 	 * @param target is the given {@link Characters} that will be affected by the newly created {@link Effect}.
 	 * @param healthDecreaseFactor is the value by which {@link Characters}'s health is decreased.
 	 * 
 	 * @return {@link Effect} 
+	 */
+	Effect createDamageEffect(Characters target, double healthDecreaseFactor);
+	/**
+	 * Creates an effect capable of poisoning a given {@link Characters}.
+	 * Poisoning means that health is decrease by a value multiple times in time.
+	 * 
+	 * @param target is the given {@link Characters} that will be affected by the newly created {@link Effect}.
+	 * @param healthDecreaseFactor is the value by which {@link Characters}'s health is decreased each time.
+	 * @return {@link Effect}
 	 */
 	Effect createPoisonEffect(Characters target, double healthDecreaseFactor);
 }
--- src/main/java/it/unibo/pensilina14/bullet/ballet/model/effects/EffectFactoryImpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/effects/EffectFactoryImpl.java
@@ -2,6 +2,11 @@ package it.unibo.pensilina14.bullet.ballet.model.effects;
 
 import it.unibo.pensilina14.bullet.ballet.model.characters.Characters;
 
+/**
+ * Implementation of an {@link Effect} factory.
+ * 
+ * {@inheritDoc}
+ */
 public class EffectFactoryImpl implements EffectFactory {
 
 	@Override
@@ -13,10 +18,18 @@ public class EffectFactoryImpl implements EffectFactory {
 	}
 
 	@Override
-	public Effect createPoisonEffect(final Characters target, final double healthDecreaseFactor) {
+	public Effect createDamageEffect(final Characters target, final double healthDecreaseFactor) {
 		final double actualHealth = target.getHealth();
 		return e -> {
 			target.setHealth(actualHealth - healthDecreaseFactor); 
+		};
+	}
+
+	@Override
+	public Effect createPoisonEffect(Characters target, double healthDecreaseFactor) {
+		final double actualHealth = target.getHealth();
+		return e -> {
+			//TODO
 		};
 	}
 
