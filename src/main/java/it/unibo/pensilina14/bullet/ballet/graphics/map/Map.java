5764fb1c146cdb61d67827baaaa6fb1a36f370fe
Luca <luca.rengo@studio.unibo.it>
2021-09-23 22:56:23 +0200

    Add Map Class.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/Map.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/Map.java
@@ -1,13 +1,6 @@
 package it.unibo.pensilina14.bullet.ballet.graphics.map;
 
-import com.badlogic.gdx.Game;
-import it.unibo.pensilina14.bullet.ballet.graphics.scenes.Scene;
-import it.unibo.pensilina14.bullet.ballet.graphics.scenes.Scene2;
-
-public class Map extends Game {
-
-    private final static int WIDTH = 1280;
-    private final static int HEIGHT = 720;
+public class Map {
 
     public enum Maps {
         HALLOWEEN("res/src/assets/Backgrounds/"),
@@ -20,50 +13,9 @@ public class Map extends Game {
         Maps(String path){
             this.path = path;
         }
+
+        public String getPath(){
+            return this.path;
+        }
     }
-
-    private final Maps map;
-
-    public Map(){
-        this.map = Maps.HALLOWEEN;
-    }
-
-    /*public Map(Maps map){
-        this.map = map;
-    }*/
-
-    @Override
-    public void create() {
-        //setScreen(new Scene());
-        setScreen(new Scene2());
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        super.resize(width, height);
-    }
-
-    @Override
-    public void pause() {
-        super.pause();
-    }
-
-    @Override
-    public void resume() {
-        super.resume();
-    }
-
-    @Override
-    public void dispose() {
-        super.dispose();
-    }
-
-    public int getWidth(){
-        return Map.WIDTH;
-    }
-
-    public int getHeight(){
-        return Map.HEIGHT;
-    }
-
 }
