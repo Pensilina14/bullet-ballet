e6d608bf7fe4067887dae8109c28cef18ae5c025
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-09-23 11:52:20 +0200

    change dimension from int to double

--- src/main/java/it/unibo/pensilina14/bullet/ballet/common/Dimension2D.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/common/Dimension2D.java
@@ -3,22 +3,23 @@ package it.unibo.pensilina14.bullet.ballet.common;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 public interface Dimension2D {
-  /**
-   * 
-   * @return object's dimensions
-   */
-  ImmutablePair<Integer, Integer> getSize();
-  
-  /**
-   * 
-   * @return object's height
-   */
-  int getHeight();
-  
-  /**
-   * 
-   * @return object's width
-   */
-  int getWidth();
+    
+    /**
+     * 
+     * @return object's dimensions
+     */
+    ImmutablePair<Double, Double> getSize();
+    
+    /**
+     * 
+     * @return object's height
+     */
+    double getHeight();
+    
+    /**
+     * 
+     * @return object's width
+     */
+    double getWidth();
     
 }
--- src/main/java/it/unibo/pensilina14/bullet/ballet/common/Dimension2Dimpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/common/Dimension2Dimpl.java
@@ -3,57 +3,56 @@ package it.unibo.pensilina14.bullet.ballet.common;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 public class Dimension2Dimpl implements Dimension2D {
-
-  private final int height;
-  private final int width;
     
-  public Dimension2Dimpl(final int height, final int width) {
-    this.height = height;
-    this.width = width;
-  }
-
-  @Override
-  public ImmutablePair<Integer, Integer> getSize() {
-    return ImmutablePair.of(this.height, this.width);
-  }
-
-  @Override
-  public int getHeight() {
-    return this.height;
-  }
-
-  @Override
-  public int getWidth() {
-    return this.width;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + height;
-    result = prime * result + width;
-    return result;
-  }
-
-  @Override
-  public boolean equals(final Object obj) {
-    if (this == obj) {
-      return true;
+    private final double height;
+    private final double width;
+      
+    public Dimension2Dimpl(final int height, final int width) {
+      this.height = height;
+      this.width = width;
     }
-    if (obj == null) {
-      return false;
+
+    @Override
+    public ImmutablePair<Double, Double> getSize() {
+      return ImmutablePair.of(this.height, this.width);
     }
-    if (getClass() != obj.getClass()) {
-      return false;
+
+    @Override
+    public double getHeight() {
+      return this.height;
     }
-    final Dimension2Dimpl other = (Dimension2Dimpl) obj;
-    if (height != other.height) {
-      return false;
+
+    @Override
+    public double getWidth() {
+      return this.width;
     }
-    if (width != other.width) {
-      return false;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(height);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(width);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
-    return true;
-  }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Dimension2Dimpl other = (Dimension2Dimpl) obj;
+        if (Double.doubleToLongBits(height) != Double.doubleToLongBits(other.height))
+            return false;
+        if (Double.doubleToLongBits(width) != Double.doubleToLongBits(other.width))
+            return false;
+        return true;
+    }
+   
 }
