2f581505b704ba6326e4aeac61b574079404b04f
Luca <luca.rengo@studio.unibo.it>
2021-09-24 18:57:12 +0200

    Fix and add background image path.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/Map.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/Map.java
@@ -3,10 +3,18 @@ package it.unibo.pensilina14.bullet.ballet.graphics.map;
 public class Map {
 
     public enum Maps {
-        HALLOWEEN("res/src/assets/Backgrounds/"),
-        JUNGLE("res/src/assets/Backgrounds/"),
-        FOREST("res/src/assets/Backgrounds/"),
-        CAVE("res/src/assets/Backgrounds/");
+        HALLOWEEN("res/assets/maps/Backgrounds/spooky_background.jpg"),
+        JUNGLE("res/assets/maps/Backgrounds/jungle_background.jpg"),
+        JUNGLE2("res/assets/maps/Backgrounds/jungle_background2.jpg"),
+        JUNGLE3("res/assets/maps/Backgrounds/jungle_background3.png"),
+        //FOREST("res/assets/maps/Backgrounds/"),
+        CAVE("res/assets/maps/Backgrounds/cave_background.png"),
+        CAVE2("res/assets/maps/Backgrounds/cave_background2.jpg"),
+        CAVE3("res/assets/maps/Backgrounds/cave_background3.jpg"),
+        LAVA("res/assets/maps/Backgrounds/lava_background.png"),
+        DESERT("res/assets/maps/Backgrounds/desert_background.jpg"),
+        DESERT2("res/assets/maps/Backgrounds/desert_background2.jpg"),
+        FUTURISTIC("res/assets/maps/Backgrounds/futuristic_background.jpg");
 
         String path;
 
