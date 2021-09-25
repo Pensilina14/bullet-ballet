ea30d7847cada3129420bb7b1aa7572dc888a906
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-09-25 09:51:09 +0200

    fix error

--- src/main/java/it/unibo/pensilina14/bullet/ballet/Launcher.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/Launcher.java
@@ -1,24 +1,14 @@
 package it.unibo.pensilina14.bullet.ballet;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import it.unibo.pensilina14.bullet.ballet.core.GameEngine;
 import it.unibo.pensilina14.bullet.ballet.graphics.map.Map;
 
 public final class Launcher {
 
 	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "Bullet Ballet";
-		cfg.useGL30 = false;
-		cfg.width = 1280;
-		cfg.height = 720;
-
-		new LwjglApplication(new Map(), cfg);
-
-		/*final GameEngine game = new GameEngine();
+		final GameEngine game = new GameEngine();
 		game.setup();
-		game.mainLoop();*/
+		game.mainLoop();
 	}
 
 }
