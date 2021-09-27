e1cb04f70b8c4c7f27d33e3ba6ce46df08af2e97
Federico Brunelli <federico.brunelli4@studio.unibo.it>
2021-09-27 17:04:29 +0200

    Add a new weapon implementation using EntityList

--- src/main/java/it/unibo/pensilina14/bullet/ballet/model/weapon/WeaponImpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/weapon/WeaponImpl.java
@@ -3,30 +3,57 @@ package it.unibo.pensilina14.bullet.ballet.model.weapon;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
-import it.unibo.pensilina14.bullet.ballet.model.effects.Effect;
 
-public class WeaponImpl implements Weapon {
+import it.unibo.pensilina14.bullet.ballet.common.Dimension2D;
+import it.unibo.pensilina14.bullet.ballet.common.SpeedVector2D;
+import it.unibo.pensilina14.bullet.ballet.model.characters.EntityList;
+import it.unibo.pensilina14.bullet.ballet.model.effects.Effect;
+import it.unibo.pensilina14.bullet.ballet.model.environment.Environment;
+
+public class WeaponImpl extends DynamicPickupItem implements Weapon {
 
 	//set for each weapon the total number of available bullets
-	private final static int MAX_AMMO = 10;
-	private final static int MAX_CAPACITY = 3;
+	private final int limitBullets;
+	private final int limitChargers;
 	private final String name;
 	private int currentAmmo;
 	private List<Bullet> spareCharger;
 	private List<List<Bullet>> bandolier;
 	
-	public WeaponImpl(String nameOfWeapon) {
+	public WeaponImpl(final String nameOfWeapon, final int limitBullets,
+			final int limitChargers, final Dimension2D dimension, final Environment gameEnvironment, 
+            final double mass, final SpeedVector2D vector, final ITEM_ID id, 
+            final Effect effect) {
+		super(dimension, gameEnvironment, mass, vector, id, effect);
+		
 		this.name = nameOfWeapon;
-		this.currentAmmo = MAX_AMMO;
-		((ArrayList<Bullet>) this.spareCharger).ensureCapacity(MAX_AMMO);
-		this.spareCharger.stream().map(i -> new BulletImpl(BulletType.CLASSICAL));
-		((ArrayList<List<Bullet>>) this.bandolier).ensureCapacity(MAX_CAPACITY);
-		this.bandolier.add(spareCharger);
-		this.bandolier.add(new ArrayList<>());
-		this.bandolier.add(new ArrayList<>());
-		//for(int i=0; i<MAX_AMMO; i++) {
+		this.limitBullets = limitBullets;
+		this.limitChargers = limitChargers;
+		this.currentAmmo = limitBullets;
+		this.initializeWeapon();
+
+		//for(int i=0; i<limitBullets; i++) {
 		//	spareCharger.add(new BulletImpl(BulletType.CLASSICAL));
 		//}
+	}
+	
+	public WeaponImpl(final EntityList.Weapons weaponType, final Dimension2D dimension, final Environment gameEnvironment, 
+            final double mass, final SpeedVector2D vector, final ITEM_ID id, 
+            final Effect effect) {
+		super(dimension, gameEnvironment, mass, vector, id, effect);
+		this.name = weaponType.getName();
+		this.limitBullets = weaponType.getLimBullets();
+		this.limitChargers = weaponType.getLimChargers();
+		this.initializeWeapon();
+	}	
+	
+	private void initializeWeapon() {
+		((ArrayList<Bullet>) this.spareCharger).ensureCapacity(this.limitBullets);
+		this.spareCharger.stream().map(i -> new BulletImpl(BulletType.CLASSICAL));
+		((ArrayList<List<Bullet>>) this.bandolier).ensureCapacity(this.limitChargers);
+		this.bandolier.add(this.spareCharger);
+		this.bandolier.add(new ArrayList<>());
+		this.bandolier.add(new ArrayList<>());
 	}
 	
 	@Override
@@ -36,7 +63,7 @@ public class WeaponImpl implements Weapon {
 	
 	@Override
 	public int getTotalAmmo() {
-		return MAX_AMMO;
+		return this.limitBullets*this.limitChargers;
 	}
 	
 	@Override
@@ -78,7 +105,7 @@ public class WeaponImpl implements Weapon {
     }
     
     private void switchCharger() {
-    	this.currentAmmo = MAX_AMMO;
+    	this.currentAmmo = limitBullets;
     	this.spareCharger = this.bandolier.stream().filter(x -> x.size()>0).findFirst().get();
     }
 
