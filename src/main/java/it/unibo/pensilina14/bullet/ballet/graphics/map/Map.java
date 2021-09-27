dc395599977ec644a32bef08bf7f8142fa1072bb
Luca <luca.rengo@studio.unibo.it>
2021-09-27 17:11:58 +0200

    Add generate() method.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/Map.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/Map.java
@@ -1,6 +1,16 @@
 package it.unibo.pensilina14.bullet.ballet.graphics.map;
 
+import javafx.util.Pair;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Map {
+
+    private final static int WIDTH = 1280;
+    private final static int HEIGHT = 720;
+
+    private final Set<Pair<Integer,Integer>> tiles = new HashSet<>();
 
     public enum Maps {
         HALLOWEEN("res/assets/maps/Backgrounds/spooky_background.jpg"),
@@ -25,5 +35,14 @@ public class Map {
         public String getPath(){
             return this.path;
         }
+    }
+
+    //TODO: metodo da tenere qui o da mettere in MapScene
+    public void generate(){
+        //TODO: posizionare a y : 0 (ovvero, HEIGHT : 0 + qualcosa) le piastrelle, capire quante piastrelle ci stanno nella larghezza della mappa
+        //TODO: quindi fare HEIGHT / larghezza_di_una_piastrella = numero_piastrelle che ci stanno nello schermo.
+        //TODO: aggiungere le coordinate di queste piastrelle in un Unmodifiable Set.
+        //TODO: però quando il player arriva a metà della mappa, la mappa si deve aggiornare mostrando nuove piastrelle (tiles) in nuove posizioni.
+        //TODO: Le posizioni delle piastrelle dovranno essere aggiunte alle entità fisiche e quindi ci sarà una collisione/fisica che fa stare il player sopra esse.
     }
 }
