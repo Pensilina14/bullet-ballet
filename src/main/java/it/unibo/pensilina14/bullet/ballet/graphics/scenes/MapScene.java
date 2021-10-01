368971f0226e4fac1140466b384c04775851f508
Luca <luca.rengo@studio.unibo.it>
2021-10-01 21:03:41 +0200

    Modify draw() method to draw platforms.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/scenes/MapScene.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/scenes/MapScene.java
@@ -1,8 +1,14 @@
 package it.unibo.pensilina14.bullet.ballet.graphics.scenes;
 
+import com.sun.tools.javac.Main;
 import it.unibo.pensilina14.bullet.ballet.graphics.map.Map;
+import it.unibo.pensilina14.bullet.ballet.graphics.sprite.MainPlayer;
+import it.unibo.pensilina14.bullet.ballet.graphics.sprite.SpriteAnimation;
+import it.unibo.pensilina14.bullet.ballet.model.characters.EntityList;
 import javafx.animation.AnimationTimer;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.paint.Color;
 
@@ -12,17 +18,27 @@ import java.nio.file.Paths;
 
 public class MapScene extends AbstractScene{
 
+    private final Map map = new Map();
+
     private Image background;
+
+    //private Node backgroundNode;
+
+    //private final MainPlayer mainPlayer = new MainPlayer(EntityList.Characters.Player.PLAYER1);
 
     public MapScene() throws IOException {
         super();
 
         this.background = new Image(Files.newInputStream(Paths.get(Map.Maps.HALLOWEEN.getPath())));
 
+        this.map.generate();
+
     }
 
     @Override
     public void draw() {
+
+        keysPressed.clear();
 
         new AnimationTimer(){
 
@@ -31,7 +47,30 @@ public class MapScene extends AbstractScene{
                 gfx.setFill(Color.BLACK);
                 gfx.fillRect(0,0,width, height);
                 gfx.drawImage(background, 0, 0);
+                // Dopo il background
+
                 //TODO: aggiungere il personaggio principale ed i nemici.
+                //gfx.drawImage(,map.getTiles().); //TODO: tiles.getX(), tiles.getY()
+
+                //TODO: HashMap
+                map.getPlatforms().forEach( (s, c) -> {
+                    gfx.drawImage(s.getSpriteImage(), c.getKey(), c.getValue()); //TODO: non mi serve sempre settare la sprite, perchè è sempra la stessa (basta una volta)
+                }); //s.getSpriteImage()
+
+                //TODO: Set
+                /*map.getTiles().forEach( (k) -> {
+                    gfx.drawImage(map.getPlatformSprite().getSprite(), k.getKey(), k.getValue());
+                });*/
+
+                if(keysPressed.contains(KeyCode.RIGHT)){
+                    //TODO: move screen pane()
+                    int offset = 100; //TODO: will be added based upon player movement
+                    root.setLayoutX(-(offset - map.getMapWidth()));
+                    //root.getChildren().addAll(backgroundNode, root, root);
+
+                    // player movement and animation
+                    //mainPlayer.move(SpriteAnimation.Direction.RIGHT);
+                }
             }
         }.start();
 
