8ca344352041bc4fce261f5436341a0b62dd91ef
Luca <luca.rengo@studio.unibo.it>
2021-10-01 21:02:11 +0200

    Add move() method to move right and left with animation.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/sprite/MainPlayer.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/sprite/MainPlayer.java
@@ -2,46 +2,74 @@ package it.unibo.pensilina14.bullet.ballet.graphics.sprite;
 
 import it.unibo.pensilina14.bullet.ballet.common.Dimension2D;
 import it.unibo.pensilina14.bullet.ballet.common.SpeedVector2D;
+import it.unibo.pensilina14.bullet.ballet.graphics.map.Map;
 import it.unibo.pensilina14.bullet.ballet.model.characters.Characters;
 import it.unibo.pensilina14.bullet.ballet.model.characters.EntityList;
+import it.unibo.pensilina14.bullet.ballet.model.characters.FactoryCharactersImpl;
 import it.unibo.pensilina14.bullet.ballet.model.characters.Player;
 import it.unibo.pensilina14.bullet.ballet.model.environment.Environment;
+import javafx.scene.image.Image;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MainPlayer extends SpriteAnimation {
 
     /*private final static int WIDTH = 64;
     private final static int HEIGHT = 64;*/
 
-    private int width;
-    private int heigth;
+    private final static int MAIN_PLAYER_WIDTH = 36; //TODO: modify values based on Sprite Width
+    private final static int MAIN_PLAYER_HEIGHT = 36; //TODO: modify values based on Sprite Width
 
     private final static int STEP = 3;
 
-    //private final static String path = "res/assets/sprites/characters/player/";
+    private static final String MAIN_PLAYER_IMAGE = "res/assets/sprites/characters/enemies/EnemyCharacterTest.png";
 
-    private final Player player;
+    private final static Map map = new Map();
 
-    public MainPlayer(int width, int height, EntityList.Characters.Player playerType, Dimension2D dimension, SpeedVector2D vector, Environment environment, double mass) {
-        //super(MainPlayer.WIDTH, MainPlayer.HEIGHT);
+    private Image mainPlayerImage; //TODO: final
 
-        super(width, height);
+    private final FactoryCharactersImpl characters = new FactoryCharactersImpl();
 
-        this.width = width;
-        this.heigth = height;
+    public MainPlayer(EntityList.Characters.Player playerType) {
 
-        this.player = new Player(playerType, dimension, vector, environment, mass);
+        super(MainPlayer.MAIN_PLAYER_WIDTH, MainPlayer.MAIN_PLAYER_HEIGHT);
+
+        //super(width, height);
+
+        /*this.MainPlayerWidth = width;
+        this.MainPlayerHeight = height;*/
+
+        Player player = characters.createPlayer(playerType);
+
+        try {
+            this.mainPlayerImage = new Image(Files.newInputStream(Paths.get(MAIN_PLAYER_IMAGE)));
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
 
         //TODO: Add sprites coords of spritesheet.
-        spriteX[Direction.RIGHT.ordinal()] = new int[] {};
-        spriteX[Direction.LEFT.ordinal()] = new int[] {};
-        spriteX[Direction.UP.ordinal()] = new int[] {};
-        spriteX[Direction.DOWN.ordinal()] = new int[] {};
+        spriteXCoords[Direction.RIGHT.ordinal()] = new int[] {};
+        spriteXCoords[Direction.LEFT.ordinal()] = new int[] {};
+        spriteXCoords[Direction.UP.ordinal()] = new int[] {};
+        spriteXCoords[Direction.DOWN.ordinal()] = new int[] {};
 
         updateSprite();
 
     }
 
     public void move(Direction direction){
-        //TODO: add movement and its relative animation by calling animate().
+        int newX = x;
+        if(direction == Direction.LEFT){
+            newX -= Math.min(MainPlayer.STEP, x);
+        }
+        else if(direction == Direction.RIGHT){
+            newX += Math.min(MainPlayer.STEP, map.getMapWidth() - MainPlayer.MAIN_PLAYER_WIDTH - x);
+        }
+        move(newX, y);
+        animate(direction);
+
+        //TODO: add up and down movements.
     }
 }
