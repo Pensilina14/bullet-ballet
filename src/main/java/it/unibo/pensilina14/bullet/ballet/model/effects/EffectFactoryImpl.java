ae269dd885c59ec3f6f5d797d2249f663470aea8
daze <leon.baiocchi@studio.unibo.it>
2021-10-04 23:04:47 +0200

    Late night mistake

--- src/main/java/it/unibo/pensilina14/bullet/ballet/model/effects/EffectFactoryImpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/effects/EffectFactoryImpl.java
@@ -1,10 +1,5 @@
 package it.unibo.pensilina14.bullet.ballet.model.effects;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-import org.apache.commons.lang3.time.StopWatch;
-
 /**
  * Implementation of an {@link Effect} factory.
  * 
@@ -28,26 +23,26 @@ public final class EffectFactoryImpl implements EffectFactory {
 		};
 	}
 
-//	@Override
-//	public Effect createPoisonEffect(final double healthDecreaseFactor, final long msStep, final long msDuration) {
-//		return e -> {
-//			new Thread() {
-//				@Override
-//				public void run() {
-//					long steppedTime = 0;
-//					while (steppedTime < msDuration) {
-//						e.setHealth(e.getHealth() - healthDecreaseFactor);
-//						try {
-//							Thread.sleep(msStep);
-//						} catch (final InterruptedException exc) {
-//							exc.printStackTrace();
-//						}
-//						steppedTime += msStep;
-//					}
-//				}
-//			}.start();
-//		};
-//	}
-//	
+	@Override
+	public Effect createPoisonEffect(final double healthDecreaseFactor, final long msStep, final long msDuration) {
+		return e -> {
+			new Thread() {
+				@Override
+				public void run() {
+					long steppedTime = 0;
+					while (steppedTime < msDuration) {
+						e.setHealth(e.getHealth() - healthDecreaseFactor);
+						try {
+							Thread.sleep(msStep);
+						} catch (final InterruptedException exc) {
+							exc.printStackTrace();
+						}
+						steppedTime += msStep;
+					}
+				}
+			}.start();
+		};
+	}
+	
 	//TODO: Create PoisonAgent in order to launch a thread well designed
 }
