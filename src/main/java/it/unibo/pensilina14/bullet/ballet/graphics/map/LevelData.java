544b858cb11f20a8a8d8e4d188306652063f3d7f
Luca <luca.rengo@studio.unibo.it>
2021-10-08 18:35:07 +0200

    Add LevelData Class to store Map's informations.

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/LevelData.java
@@ -0,0 +1,23 @@
+package it.unibo.pensilina14.bullet.ballet.graphics.map;
+
+public class LevelData {
+
+    //TODO: metterli in un enum?
+    //TODO: fare un metodo per ottenre un level a caso.
+    //TODO: aggiungere gli altri numeri per gli altri oggetti.
+
+    public static final String[] LEVEL1 = new String[] {
+            "000000000000000000000000000000",
+            "000000000000000000000000000000",
+            "000000000000000000000000000000",
+            "000000000000000000000000000000",
+            "000000000000000000000000000000",
+            "000000000000000000000000000000",
+            "000000000000000000000000000000",
+            "000111000000000000000000000000",
+            "000000001110000000000000000000",
+            "000002000000011100002000000000",
+            "000001110000000000011100011000",
+            "111111110011110001111100111111"
+    };
+}
