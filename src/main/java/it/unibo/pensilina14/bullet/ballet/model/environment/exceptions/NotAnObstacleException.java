c4c0bdbfdf025e87f28fecb63bd9cdcb2f8f74e4
daze <leon.baiocchi@studio.unibo.it>
2021-10-09 23:08:53 +0200

    Add exception

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/environment/exceptions/NotAnObstacleException.java
@@ -0,0 +1,10 @@
+package it.unibo.pensilina14.bullet.ballet.model.environment.exceptions;
+
+public class NotAnObstacleException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 647040467782697252L;
+
+}
