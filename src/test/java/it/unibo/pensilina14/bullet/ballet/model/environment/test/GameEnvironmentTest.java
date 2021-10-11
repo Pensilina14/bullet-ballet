27730e45dbbaa5be9a66f7ef3286ea4562a9110d
daze <leon.baiocchi@studio.unibo.it>
2021-10-11 21:41:13 +0200

    Basic testing done

--- src/test/java/it/unibo/pensilina14/bullet/ballet/model/environment/test/GameEnvironmentTest.java
+++ src/test/java/it/unibo/pensilina14/bullet/ballet/model/environment/test/GameEnvironmentTest.java
@@ -8,15 +8,28 @@ import it.unibo.pensilina14.bullet.ballet.common.ImmutablePosition2D;
 import it.unibo.pensilina14.bullet.ballet.common.ImmutablePosition2Dimpl;
 import it.unibo.pensilina14.bullet.ballet.common.MutablePosition2Dimpl;
 import it.unibo.pensilina14.bullet.ballet.common.SpeedVector2DImpl;
+import it.unibo.pensilina14.bullet.ballet.model.characters.EntityList;
+import it.unibo.pensilina14.bullet.ballet.model.characters.FactoryCharacters;
+import it.unibo.pensilina14.bullet.ballet.model.characters.FactoryCharactersImpl;
+import it.unibo.pensilina14.bullet.ballet.model.characters.Player;
 import it.unibo.pensilina14.bullet.ballet.model.effects.EffectFactory;
 import it.unibo.pensilina14.bullet.ballet.model.effects.EffectFactoryImpl;
 import it.unibo.pensilina14.bullet.ballet.model.entities.PhysicalObject;
 import it.unibo.pensilina14.bullet.ballet.model.environment.Environment;
+import it.unibo.pensilina14.bullet.ballet.model.environment.Environment.GravityConstants;
 import it.unibo.pensilina14.bullet.ballet.model.environment.GameEnvironment;
+import it.unibo.pensilina14.bullet.ballet.model.obstacle.DynamicObstacle;
+import it.unibo.pensilina14.bullet.ballet.model.obstacle.ObstacleFactory;
+import it.unibo.pensilina14.bullet.ballet.model.obstacle.ObstacleFactoryImpl;
 import it.unibo.pensilina14.bullet.ballet.model.weapon.DynamicPickupItem;
 import it.unibo.pensilina14.bullet.ballet.model.weapon.ITEM_ID;
+import it.unibo.pensilina14.bullet.ballet.model.weapon.Item;
+import it.unibo.pensilina14.bullet.ballet.model.weapon.ItemFactory;
+import it.unibo.pensilina14.bullet.ballet.model.weapon.ItemFactoryImpl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -24,107 +37,56 @@ import java.util.Set;
 import org.junit.Test;
 
 public class GameEnvironmentTest {
-  private static final double EARTH_GRAVITY = 9.81;
-  private static final double MOON_GRAVITY = 6.673;
-  private static final int DEFAULT_DIM = 500;
+  private static final int DEFAULT_DIM = 50;
   private static final double DELTA = 0.01;
   private static final double DEFAULT_MASS = 10.0;
   private static final double DEFAULT_SPEED = 15.0;
+  
+  private final FactoryCharacters characterFactory = new FactoryCharactersImpl();
+  private final ObstacleFactory obstacleFactory = new ObstacleFactoryImpl();
 
   @Test
   public void testGameEnvironment() {
     final Environment gameEnv = new GameEnvironment();
-
-    assertEquals(gameEnv.getGravity(), EARTH_GRAVITY, DELTA);
-    assertEquals(gameEnv.getDimension(), new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM));
-
-    final Map<ImmutablePosition2D, Optional<PhysicalObject>> expectedMap = new HashMap<>();
-    for (int x = 0; x < gameEnv.getDimension().getWidth(); x++) {
-      for (int y = 0; y < gameEnv.getDimension().getHeight(); y++) {
-        expectedMap.put(new ImmutablePosition2Dimpl(x, y), Optional.empty());
-      }
-    }
-
-    assertEquals(gameEnv.getMap(), expectedMap);
+    
+    assertEquals(gameEnv.getGravity(), GravityConstants.EARTH.getValue(), DELTA);
+    assertTrue(gameEnv.getPlayer().isEmpty());
+    assertEquals(gameEnv.getEnemies().get(), new ArrayList<>());
+    assertEquals(gameEnv.getObstacles().get(), new ArrayList<>());
+    assertEquals(gameEnv.getItems().get(), new ArrayList<>());
   }
 
   @Test
   public void testGameEnvironmentWithArgs() {
-    final Environment gameEnv = new GameEnvironment(MOON_GRAVITY, 
-        new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM));
+	final Player player = this.characterFactory.createPlayer(EntityList.Characters.Player.PLAYER1);
+    final Environment gameEnv = new GameEnvironment(GravityConstants.MOON.getValue(), Optional.of(player));
 
-    assertEquals(gameEnv.getGravity(), MOON_GRAVITY, DELTA);
-    assertEquals(gameEnv.getDimension(), new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM));
-
-    final Map<ImmutablePosition2D, Optional<PhysicalObject>> expectedMap = new HashMap<>();
-    for (int x = 0; x < gameEnv.getDimension().getWidth(); x++) {
-      for (int y = 0; y < gameEnv.getDimension().getHeight(); y++) {
-        expectedMap.put(new ImmutablePosition2Dimpl(x, y), Optional.empty());
-      }
-    }
-
-    assertEquals(gameEnv.getMap(), expectedMap);
-  }
-
-  @Test
-  public void testAddObjToMap() {
-    final Environment gameEnv = new GameEnvironment();
-    final PhysicalObject obj = new DynamicPickupItem(
-            new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM),
-            gameEnv, DEFAULT_MASS, new SpeedVector2DImpl(new MutablePosition2Dimpl(0, 0), DEFAULT_SPEED),
-            ITEM_ID.HEART, null); // TODO: substitution of null!!!
-    final ImmutablePosition2D pos = new ImmutablePosition2Dimpl(0, 0);
-
-    final boolean hasBeenAdded = gameEnv.addObjToMap(obj, pos);
-
-    assertTrue(hasBeenAdded);
-    assertEquals(gameEnv.findObjInMap(obj).get(), pos);
+    assertEquals(gameEnv.getGravity(), GravityConstants.MOON.getValue(), DELTA);
+    assertEquals(gameEnv.getPlayer(), Optional.of(player));
+    assertEquals(gameEnv.getEnemies().get(), new ArrayList<>());
+    assertEquals(gameEnv.getObstacles().get(), new ArrayList<>());
+    assertEquals(gameEnv.getItems().get(), new ArrayList<>());
   }
 
   @Test
   public void testDeleteObjByPosition() {
-    final Environment gameEnv = new GameEnvironment();
-    final PhysicalObject obj = new DynamicPickupItem(
-            new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM),
-            gameEnv, DEFAULT_MASS, new SpeedVector2DImpl(new MutablePosition2Dimpl(0, 0), DEFAULT_SPEED),
-            ITEM_ID.HEART, null); // TODO: substitution of null!!!
-    final ImmutablePosition2D pos = new ImmutablePosition2Dimpl(0, 0);
-
-    final boolean hasBeenAdded = gameEnv.addObjToMap(obj, pos);
-    gameEnv.deleteObjByPosition(pos);
-
-    assertTrue(hasBeenAdded);
-    assertTrue(gameEnv.findObjInMap(obj).isEmpty());
-  }
-
-  @Test
-  public void testGetObjListInMap() {
-	  final EffectFactory factory = new EffectFactoryImpl();
-      final Environment gameEnv = new GameEnvironment();
-      final PhysicalObject obj = new DynamicPickupItem(
-              new Dimension2Dimpl(DEFAULT_DIM, DEFAULT_DIM),
-              gameEnv, DEFAULT_MASS, new SpeedVector2DImpl(new MutablePosition2Dimpl(0, 0), DEFAULT_SPEED),
-              ITEM_ID.HEART, factory.createHealEffect(10)); 
-      final PhysicalObject obj2 = new DynamicPickupItem(
-              new Dimension2Dimpl(DEFAULT_DIM + 1, DEFAULT_DIM + 1),
-              gameEnv, DEFAULT_MASS + 1, new SpeedVector2DImpl(new MutablePosition2Dimpl(0, 1), DEFAULT_SPEED),
-              ITEM_ID.HEART, factory.createHealEffect(10)); 
-      final PhysicalObject obj3 = new DynamicPickupItem(
-              new Dimension2Dimpl(DEFAULT_DIM + 2, DEFAULT_DIM + 2),
-              gameEnv, DEFAULT_MASS + 2, new SpeedVector2DImpl(new MutablePosition2Dimpl(1, 1), DEFAULT_SPEED),
-              ITEM_ID.HEART, factory.createHealEffect(10)); 
-
-      final boolean firstIsAdded = gameEnv.addObjToMap(obj, 
-              new ImmutablePosition2Dimpl(obj.getPosition().getX(), obj.getPosition().getY()));
-      final boolean secondIsAdded = gameEnv.addObjToMap(obj2,
-              new ImmutablePosition2Dimpl(obj2.getPosition().getX(), obj2.getPosition().getY()));
-      final boolean thirdIsAdded = gameEnv.addObjToMap(obj3,
-              new ImmutablePosition2Dimpl(obj3.getPosition().getX(), obj3.getPosition().getY()));
-
-      assertTrue(firstIsAdded);
-      assertTrue(secondIsAdded);
-      assertTrue(thirdIsAdded);
-      assertTrue(gameEnv.getObjSetInMap().isPresent());
-      assertEquals(Set.of(obj, obj2, obj3), gameEnv.getObjSetInMap().get());
+	final Player player = this.characterFactory.createPlayer(EntityList.Characters.Player.PLAYER1);
+	final Environment gameEnv = new GameEnvironment(GravityConstants.MOON.getValue(), Optional.of(player));
+	
+	final ImmutablePosition2D pos = new ImmutablePosition2Dimpl(0, 0);
+	gameEnv.deleteObjByPosition(pos);
+	
+	final ImmutablePosition2D pos2 = new ImmutablePosition2Dimpl(100, 0);
+	final DynamicObstacle obstacle = this.obstacleFactory.createDynamicObstacle(gameEnv, 
+			new SpeedVector2DImpl(new MutablePosition2Dimpl(100, 0), DEFAULT_SPEED));
+	gameEnv.addObstacle(obstacle);
+	final Optional<List<PhysicalObject>> obstaclesBefore = gameEnv.getObstacles();
+	gameEnv.deleteObjByPosition(pos2);
+	final Optional<List<PhysicalObject>> obstaclesAfter = gameEnv.getObstacles();
+	
+	assertTrue(gameEnv.getPlayer().isEmpty());
+	assertEquals(obstaclesBefore, Optional.of(List.of(obstacle)));
+	System.out.println(obstaclesAfter);
+	assertTrue(obstaclesAfter.isPresent());
   }
 }
