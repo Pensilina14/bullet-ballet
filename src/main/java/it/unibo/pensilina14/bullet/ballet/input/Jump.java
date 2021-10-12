8d9524b2780e18492cb8a8c7eb2f1ee5f6865334
daze <leon.baiocchi@studio.unibo.it>
2021-10-12 18:20:26 +0200

    Implement method execute

--- src/main/java/it/unibo/pensilina14/bullet/ballet/input/Jump.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/input/Jump.java
@@ -1,12 +1,14 @@
 package it.unibo.pensilina14.bullet.ballet.input;
 
+import it.unibo.pensilina14.bullet.ballet.model.characters.Player;
 import it.unibo.pensilina14.bullet.ballet.model.environment.GameState;
 
 public class Jump implements Command {
 
 	@Override
-	public void execute(final GameState env) {
-		// TODO Auto-generated method stub
+	public final void execute(final GameState env) {
+		final Player player = env.getGameEnvironment().getPlayer().get();
+		player.jump();
 	}
 
 }
