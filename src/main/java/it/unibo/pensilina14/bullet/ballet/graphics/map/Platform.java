7f2fa869c3e8a568b86daca77fff66d7c7175c75
Luca <luca.rengo@studio.unibo.it>
2021-10-15 21:01:42 +0200

    Add more platforms types and relative images.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/Platform.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/map/Platform.java
@@ -14,10 +14,14 @@ public class Platform extends Pane { //TODO: Lasciare tutto qui o mettere tutto 
 
     public enum Platforms { //TODO: Add paths of other platform images.
         HALLOWEEN_PLATFORM("res/assets/maps/Tiles/halloween_tile.png"),
-        CAVE_PLATFORM(""),
+        CAVE_PLATFORM("res/assets/maps/Tiles/rock_tile2.png"),
+        CAVE_PLATFORM2("res/assets/maps/Tiles/rock_tile4.png"),
+        CAVE_PLATFORM3("res/assets/maps/Tiles/rock_tile.jpg"),
         JUNGLE_PLATFORM(""),
-        LAVA_PLATFORM(""),
+        SWAMP_PLATFORM("res/assets/maps/Tiles/Swamp-tile.jpg"),
+        LAVA_PLATFORM("res/assets/maps/Tiles/brick_wall-red.png"), //TODO: change it
         DESERT_PLATFORM("res/assets/maps/Tiles/tmw_desert_spacing.png"),
+        DESERT_PLATFORM2("res/assets/maps/Tiles/desert-tile.png"),
         FUTURISTIC_PLATFORM(""),
         ICE_PLATFORM("res/assets/maps/Tiles/ice_tile3.png");
 
@@ -54,6 +58,7 @@ public class Platform extends Pane { //TODO: Lasciare tutto qui o mettere tutto 
 
     private void setPlatform(int x, int y) throws IOException {
         switch(this.platformType){
+            //TODO: settare uno per tutti.
             case DESERT_PLATFORM:
                 minX = 20;
                 minY = 20;
@@ -66,6 +71,16 @@ public class Platform extends Pane { //TODO: Lasciare tutto qui o mettere tutto 
                 /*this.platformView.setFitWidth(MapScene.MAP_LENGTH);
                 this.platformView.setFitHeight(MapScene.MAP_LENGTH);*/
 
+                this.platformView.setViewport(new Rectangle2D(minX,minY,platformWidth,platformHeight));
+                break;
+            case DESERT_PLATFORM2:
+                minX = 0;
+                minY = 0;
+                platformWidth = 60;
+                platformHeight = 60;
+                this.platformView = new ImageView(new Image(Files.newInputStream(Paths.get(Platforms.DESERT_PLATFORM2.getPath()))));
+                this.setTranslateX(x);
+                this.setTranslateY(y);
                 this.platformView.setViewport(new Rectangle2D(minX,minY,platformWidth,platformHeight));
                 break;
                 //TODO: Add other platform types
@@ -84,8 +99,39 @@ public class Platform extends Pane { //TODO: Lasciare tutto qui o mettere tutto 
             case JUNGLE_PLATFORM:
                 break;
             case LAVA_PLATFORM:
+                minX = 0;
+                minY = 0;
+                platformWidth = 60;
+                platformHeight = 60;
+                this.platformView = new ImageView(new Image(Files.newInputStream(Paths.get(Platforms.LAVA_PLATFORM.getPath()))));
+                this.setTranslateX(x);
+                this.setTranslateY(y);
+
                 break;
             case CAVE_PLATFORM:
+                minX = 0;
+                minY = 0;
+                platformWidth = 60;
+                platformHeight = 60;
+                this.platformView = new ImageView(new Image(Files.newInputStream(Paths.get(Platforms.CAVE_PLATFORM.getPath()))));
+                this.setTranslateX(x);
+                this.setTranslateY(y);
+
+                this.platformView.setFitWidth(MapScene.PLATFORM_SIZE);
+                this.platformView.setFitHeight(MapScene.PLATFORM_SIZE);
+
+                break;
+            case CAVE_PLATFORM3:
+                minX = 0;
+                minY = 0;
+                platformWidth = 60;
+                platformHeight = 60;
+                this.platformView = new ImageView(new Image(Files.newInputStream(Paths.get(Platforms.CAVE_PLATFORM3.getPath()))));
+                this.setTranslateX(x);
+                this.setTranslateY(y);
+
+                this.platformView.setFitWidth(MapScene.PLATFORM_SIZE);
+                this.platformView.setFitHeight(MapScene.PLATFORM_SIZE);
                 break;
             case ICE_PLATFORM:
                 minX = 0;
@@ -95,6 +141,19 @@ public class Platform extends Pane { //TODO: Lasciare tutto qui o mettere tutto 
                 this.platformView = new ImageView(new Image(Files.newInputStream(Paths.get(Platforms.ICE_PLATFORM.getPath()))));
                 this.setTranslateX(x);
                 this.setTranslateY(y);
+
+                break;
+            case SWAMP_PLATFORM:
+                minX = 0;
+                minY = 0;
+                platformWidth = 60;
+                platformHeight = 60;
+                this.platformView = new ImageView(new Image(Files.newInputStream(Paths.get(Platforms.SWAMP_PLATFORM.getPath()))));
+                this.setTranslateX(x);
+                this.setTranslateY(y);
+
+                this.platformView.setFitWidth(MapScene.PLATFORM_SIZE);
+                this.platformView.setFitHeight(MapScene.PLATFORM_SIZE);
 
                 break;
             default:
