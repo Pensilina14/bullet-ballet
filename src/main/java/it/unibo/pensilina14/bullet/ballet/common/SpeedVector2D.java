12e299adae49ccf36605b3ce94f4ee5f2304ca89
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-10-16 15:05:01 +0200

    add comments

--- src/main/java/it/unibo/pensilina14/bullet/ballet/common/SpeedVector2D.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/common/SpeedVector2D.java
@@ -2,9 +2,28 @@ package it.unibo.pensilina14.bullet.ballet.common;
 
 public interface SpeedVector2D {
 
+    /**
+     * 
+     * @return speed value
+     */
     double getSpeed();
+    /**
+     * This method moves an object, it also computes speed.
+     * 
+     * @param x represents movement in x axis direction
+     * @param y represents movement in y axis direction
+     */
     void vectorSum(double x, double y);
+    /**
+     * This method moves an object, it doesn't calculate speed.
+     * 
+     * @param x represents movement in x axis direction
+     * @param y represents movement in y axis direction
+     */
     void noSpeedVectorSum(double x, double y);
+    /**
+     * @return vector's position
+     */
     MutablePosition2D getPosition();
     
 }
