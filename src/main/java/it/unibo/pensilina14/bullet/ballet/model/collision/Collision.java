580524f702d5b7884008c4ef4c91c38b48522157
FedericoBrunelli <federico.brunelli4@studio.unibo.it>
2021-10-16 11:33:25 +0200

    Add new implementation of collision

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/collision/Collision.java
@@ -0,0 +1,11 @@
+package it.unibo.pensilina14.bullet.ballet.model.collision;
+
+import it.unibo.pensilina14.bullet.ballet.model.entities.PhysicalObject;
+
+public interface Collision {
+
+	/*
+	 *  @return true if the first object collides with the second.
+	 */
+	boolean isCollidingWith(PhysicalObject otherObject);
+}
