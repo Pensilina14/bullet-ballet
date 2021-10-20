409c9cd6554ce3e43170ca3d84360ef5c132b203
daze <leon.baiocchi@studio.unibo.it>
2021-10-20 17:38:20 +0200

    Implement AppLogger to resolve problems

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/logging/AppLogger.java
@@ -0,0 +1,47 @@
+package it.unibo.pensilina14.bullet.ballet.logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+
+public final class AppLogger {
+	
+	private static final AppLogger SINGLETON = new AppLogger();
+	private final Logger logger;
+	
+	private AppLogger() {
+		this.logger = (Logger) LogManager.getLogger("BulletBallet");
+
+		final ConsoleAppender consoleAppender = ConsoleAppender.newBuilder()
+				.setLayout(PatternLayout.newBuilder()
+						.withPattern("%d [%p|%c|%C{1}] %m%n")
+						.build())
+				.build();
+		this.logger.addAppender(consoleAppender);
+	}
+	
+	public static AppLogger getAppLogger() {
+		return AppLogger.SINGLETON;
+	}
+	
+	public void debug(final String msg) {
+		this.logger.debug(msg);
+	}
+	
+	public void info(final String msg) {
+		this.logger.info(msg);
+	}
+	
+	public void warn(final String msg) {
+		this.logger.warn(msg);
+	}
+	
+	public void error(final String msg) {
+		this.logger.error(msg);
+	}
+	
+	public void fatal(final String msg) {
+		this.logger.fatal(msg);
+	}
+}
