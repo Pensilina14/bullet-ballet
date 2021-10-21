18b13aed964bc48ea3ed425164661200ca9f1d61
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-10-21 12:16:28 +0200

    add mock data to table view

--- src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/GameStatsController.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/GameStatsController.java
@@ -1,14 +1,23 @@
 package it.unibo.pensilina14.bullet.ballet.menu.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
-public class GameStatsController {
+public class GameStatsController implements Initializable{
 
     private final PageLoader loader = new PageLoaderImpl();
+    @FXML
+    private TableView<Statistics> tableView;
     @FXML
     private TableColumn<Statistics, String> playerName;
     @FXML
@@ -19,6 +28,20 @@ public class GameStatsController {
     @FXML
     void goBackOnMouseClick(final MouseEvent event) throws IOException {
         loader.goToSelectedPageOnInput(Frames.HOMEPAGE, event);
+    }
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        this.setCells();
+        final ObservableList<Statistics> list = FXCollections.observableArrayList();
+        list.add(new Statistics("Alessandro", 10, 0.5));
+        tableView.setItems(list);
+    }
+    
+    private void setCells() {
+        this.playerName.setCellValueFactory(new PropertyValueFactory<Statistics, String>("playerName"));
+        this.points.setCellValueFactory(new PropertyValueFactory<Statistics, Integer>("points"));
+        this.time.setCellValueFactory(new PropertyValueFactory<Statistics, Double>("time"));
     }
 
 }
