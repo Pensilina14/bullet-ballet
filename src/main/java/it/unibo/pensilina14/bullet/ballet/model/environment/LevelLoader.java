0bceeb038dc72c92f39ebc41a7aaa7a8fa43b5ba
Luca <luca.rengo@studio.unibo.it>
2021-10-23 22:23:18 +0200

    Encapsulate LevelLoader Class.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/model/environment/LevelLoader.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/environment/LevelLoader.java
@@ -9,37 +9,32 @@ import java.util.Random;
 import java.util.logging.Level;
 
 public class LevelLoader {
-    //TODO: da encapsulare.
 
-    private final static Save levelData = new Save();
-    public static final String[] level = levelData.loadLevel(getRandomLevel());
+    private final Save levelData;
+    private final String[] level;
 
-    /**
-     * 
-     */
-    /*public static String[][] levels= new String[][]{
-            LEVEL0, LEVEL1, LEVEL2
-    };*/
+    private final int LEVEL_WIDTH;
+    //private final int MAX_LEVELS;
 
-    /**
-     * 
-     */
-    public static final int MAX_LEVELS = 3; //TODO: nella classe Save prendere quanti files ci sono nella directory levels per determinare il numero di MAX_LEVELS
-    /**
-     * 
-     * 
-     */
-    //public static final int LEVEL_WIDTH = LevelData.levels[LevelData.getRandomLevel()][0].length();
-    public static final int LEVEL_WIDTH = level[0].length();
-
-    public static int getRandomLevel(){
-        final Random rand = new Random();
-        final int min = 0;
-        return rand.nextInt(((MAX_LEVELS - min)) + min); //TODO: min ridondante in questo caso.
+    public LevelLoader(){
+       this.levelData = new Save();
+       this.level = levelData.loadLevel(getRandomLevel());
+       this.LEVEL_WIDTH = this.level[0].length();
+       //this.MAX_LEVELS = this.levelData.getNumberOfLevels(); // da 0, non so perchè
     }
 
-    /*public String[] getLevel(){
-        return LevelData.level;
-    }*/
+    private int getRandomLevel(){
+        final Random rand = new Random();
+        final int maxLevels = this.levelData.getNumberOfLevels();
+        return rand.nextInt(maxLevels);
+    }
+
+    public int getLevelWidth(){
+        return this.LEVEL_WIDTH;
+    }
+
+    public String[] getLevel(){
+        return this.level;
+    }
 
 }
