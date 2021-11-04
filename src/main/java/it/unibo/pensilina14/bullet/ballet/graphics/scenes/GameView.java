cbddfc909d4f0d42f889aee8dab073bb1fa6ed37
daze <leon.baiocchi@studio.unibo.it>
2021-11-04 15:23:40 +0100

    Refactoring

--- src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/scenes/GameView.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/scenes/GameView.java
@@ -3,7 +3,15 @@ package it.unibo.pensilina14.bullet.ballet.graphics.scenes;
 import it.unibo.pensilina14.bullet.ballet.input.Controller;
 import javafx.scene.layout.Pane;
 
+/**
+ * Describes a contract for every game view
+ * class that has to work as the game frame.
+ *
+ */
 public interface GameView {
+	/**
+	 * 
+	 */
 	void setup();
 	void draw();
 	void setInputController(Controller controller);
