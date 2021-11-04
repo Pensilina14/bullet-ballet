9d55f95858b05b6143027b2a9e6f06fc636c4ae5
AlePioggia <alessandro.pioggia2@studio.unibo.it>
2021-11-04 18:24:37 +0100

    add difficulties

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/Difficulties.java
@@ -0,0 +1,23 @@
+package it.unibo.pensilina14.bullet.ballet.menu.controller;
+
+public enum Difficulties {
+
+	EASY("easy"),
+	MEDIUM("medium"),
+	HARD("hard");
+	
+	private final String difficulty;
+
+	private Difficulties(String difficulty) {
+		this.difficulty = difficulty;
+	}
+	
+	/* 
+     * @see java.lang.Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return this.difficulty;
+    }
+	
+}
