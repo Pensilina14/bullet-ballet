76a71c1d6f0b3d52e3986802d3f0f9c5870f592d
FedericoBrunelli <federico.brunelli4@studio.unibo.it>
2021-12-21 11:30:41 +0100

    Add escape and space commands

---
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/input/Esc.java
@@ -0,0 +1,30 @@
+package it.unibo.pensilina14.bullet.ballet.input;
+
+import java.io.IOException;
+
+import it.unibo.pensilina14.bullet.ballet.menu.controller.Frames;
+import it.unibo.pensilina14.bullet.ballet.menu.controller.PageLoader;
+import it.unibo.pensilina14.bullet.ballet.menu.controller.PageLoaderImpl;
+import it.unibo.pensilina14.bullet.ballet.menu.controller.PauseMenuController;
+import it.unibo.pensilina14.bullet.ballet.model.environment.GameState;
+
+public class Esc implements Command {
+
+	private final PageLoader loader;
+	
+	public Esc() {
+		this.loader = new PageLoaderImpl();
+	}
+	@Override
+	public void execute(final GameState env) {
+		try {
+			loader.goToSelectedPageOnInput(Frames.PAUSEMENU);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
