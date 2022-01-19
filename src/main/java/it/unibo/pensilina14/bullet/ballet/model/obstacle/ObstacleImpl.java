5ee818a6b1a2b7bbf350573bc83a22da17803acd
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2022-01-19 11:46:50 +0100

    add comments for next work session

--- src/main/java/it/unibo/pensilina14/bullet/ballet/model/obstacle/ObstacleImpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/obstacle/ObstacleImpl.java
@@ -1,5 +1,7 @@
 package it.unibo.pensilina14.bullet.ballet.model.obstacle;
 
+import java.util.Random;
+
 import it.unibo.pensilina14.bullet.ballet.common.Dimension2D;
 import it.unibo.pensilina14.bullet.ballet.common.SpeedVector2D;
 import it.unibo.pensilina14.bullet.ballet.model.entities.GameEntity;
@@ -7,6 +9,8 @@ import it.unibo.pensilina14.bullet.ballet.model.environment.Environment;
 
 public class ObstacleImpl extends GameEntity implements Obstacle{
 
+	//private Random random = new Random();
+	
 	public ObstacleImpl(final SpeedVector2D speedVector, final Environment gameEnvironment
 			, final double mass, final Dimension2D dimension) {
 		super(speedVector, gameEnvironment, mass, dimension);
@@ -14,7 +18,21 @@ public class ObstacleImpl extends GameEntity implements Obstacle{
 
 	@Override
 	public void rotate() {
-		// TODO implement this cool method
+		/*
+		final double rand = random.nextDouble();
+		if (rand % 2 == 0) {
+			this.moveRight(5);
+			System.out.println("right");
+		} else {
+			this.moveLeft(5);
+			System.out.println("left");
+		}
+		*/
 	}
-	
+
+	@Override
+	public void updateState() {
+		//this.rotate();
+	}
+
 }
