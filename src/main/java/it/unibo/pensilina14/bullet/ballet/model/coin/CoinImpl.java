aca0531650fc06448c6e4144bbfce4bed29ab079
Luca <luca.rengo@studio.unibo.it>
2022-01-19 16:07:46 +0100

    Update Coin to extends PickUpItem.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/model/coin/CoinImpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/model/coin/CoinImpl.java
@@ -7,11 +7,12 @@ import it.unibo.pensilina14.bullet.ballet.model.effects.Effect;
 import it.unibo.pensilina14.bullet.ballet.model.entities.GameEntity;
 import it.unibo.pensilina14.bullet.ballet.model.environment.Environment;
 import it.unibo.pensilina14.bullet.ballet.model.weapon.Items;
+import it.unibo.pensilina14.bullet.ballet.model.weapon.PickupItem;
 
 import java.util.Optional;
 import java.util.Random;
 
-public class CoinImpl extends GameEntity implements Coin {
+public class CoinImpl extends PickupItem implements Coin {
 
     //TODO: o coin con un unico valore o con valori randomici oppure valore in base alla tipologia.
     //TODO: però magari non ha senso avere un coin che valga in modo randomico, ha più senso che un coin valga 1
@@ -19,8 +20,9 @@ public class CoinImpl extends GameEntity implements Coin {
     private final static int COIN_VALUE = 1;
     private final static int MAX_COIN_VALUE = 5;
 
-    public CoinImpl(final SpeedVector2D speedVector, final Environment gameEnvironment, final double mass, final Dimension2D dimension){ //TODO: Item itemID e l'effect come parametro?
-        super(speedVector, gameEnvironment, mass, dimension);
+    public CoinImpl(final SpeedVector2D speedVector, final Environment gameEnvironment, final double mass, final Dimension2D dimension,
+                    final Items itemID, final Effect effect){
+        super(speedVector, gameEnvironment, mass, dimension, itemID, effect);
     }
 
     @Override
