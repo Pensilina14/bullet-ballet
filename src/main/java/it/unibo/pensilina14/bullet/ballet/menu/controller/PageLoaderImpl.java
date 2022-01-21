9a8e954b2c7ae05307f136aeda5a9fa0895834e3
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2022-01-21 12:08:37 +0100

    refactor and add new methods

--- src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/PageLoaderImpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/PageLoaderImpl.java
@@ -2,7 +2,6 @@ package it.unibo.pensilina14.bullet.ballet.menu.controller;
 
 import java.io.IOException;
 import java.util.Locale;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 import it.unibo.pensilina14.bullet.ballet.save.Save;
@@ -14,20 +13,21 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class PageLoaderImpl implements PageLoader{
-
-    //final Locale currentLocale = Locale.getDefault(); // Questo serve per ottenere il paese in cui ci troviamo, se vogliamo basare la lingua sul paese di provenienza.
     
     private static final String LANGUAGES_PATH = "languages.bulletBallet";
-    private static final String DEFAULT_LANGUAGE = "en_UK";
-
-    private final String language = (!Save.loadSettings().isEmpty()) ? Save.loadSettings().get(Save.LANGUAGE_STRING) : PageLoaderImpl.DEFAULT_LANGUAGE;
-
-    private final Locale locale = new Locale(this.language);
-    private final ResourceBundle bundle = ResourceBundle.getBundle(PageLoaderImpl.LANGUAGES_PATH, locale);
 
     @Override
+	public void goToSelectedPageOnInput(final Frames frame, final MouseEvent event, final Languages language) throws IOException {
+    	final Parent root = FXMLLoader.load(getClass().getResource(frame.toString()), getLanguage(language));
+        final Scene scene = new Scene(root);
+        final Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
+        window.setScene(scene);
+        window.show();
+	}
+    
+    @Override
     public void goToSelectedPageOnInput(final Frames frame, final MouseEvent event) throws IOException {
-        final Parent root = FXMLLoader.load(getClass().getResource(frame.toString()), this.bundle);
+        final Parent root = FXMLLoader.load(getClass().getResource(frame.toString()), getLanguage(Languages.ENGLISH));
         final Scene scene = new Scene(root);
         final Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
         window.setScene(scene);
@@ -36,7 +36,7 @@ public class PageLoaderImpl implements PageLoader{
     
     @Override
     public void goToSelectedPageOnInput(final Frames frame) throws IOException { 
-    	final Parent root = FXMLLoader.load(getClass().getResource(frame.toString()), this.bundle);
+    	final Parent root = FXMLLoader.load(getClass().getResource(frame.toString()), getLanguage(Languages.ENGLISH));
     	final Scene scene = new Scene(root);
     	final Stage window = new Stage();
     	window.setScene(scene);
@@ -44,12 +44,33 @@ public class PageLoaderImpl implements PageLoader{
     }
     
     @Override
+	public void loadFirstScene(final Stage primaryStage, final Languages language) throws IOException {
+    	final Parent root = FXMLLoader.load(getClass().getResource(Frames.HOMEPAGE.toString()), getLanguage(language));
+        final Scene scene = new Scene(root);
+        primaryStage.setTitle("bullet-ballet");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+    
+    @Override
     public void loadFirstScene (final Stage primaryStage) throws IOException{
-        final Parent root = FXMLLoader.load(getClass().getResource(Frames.HOMEPAGE.toString()), this.bundle);
+        final Parent root = FXMLLoader.load(getClass().getResource(Frames.HOMEPAGE.toString()), getLanguage(Languages.ENGLISH));
         final Scene scene = new Scene(root);
         primaryStage.setTitle("bullet-ballet");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    
+   private ResourceBundle getLanguage(final Languages language) {
+	    final Locale locale = new Locale(composeLanguageString(language));
+	    return ResourceBundle.getBundle(PageLoaderImpl.LANGUAGES_PATH, locale);
+    }
+    
+    private String composeLanguageString(final Languages language) {
+    	return !Save.loadSettings().isEmpty() ? Save.loadSettings().get(Save.LANGUAGE_STRING) : language.getCountryCode().toString();
+    }
+
+	
+
     
 }
