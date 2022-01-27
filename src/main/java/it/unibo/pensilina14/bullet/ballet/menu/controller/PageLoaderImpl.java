dedce38005781a427b60a7641a6d046e1edc0c96
Luca <luca.rengo@studio.unibo.it>
2022-01-27 22:42:58 +0100

    Add methods to pass screen width and height.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/PageLoaderImpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/PageLoaderImpl.java
@@ -17,6 +17,8 @@ public class PageLoaderImpl implements PageLoader{
     
     private static final String LANGUAGES_PATH = "languages.bulletBallet";
 
+    //TODO: volendo si possono unire in un metodo quelli con la lingua e quelli con la width e la height.
+
     @Override
 	public void goToSelectedPageOnInput(final Frames frame, final MouseEvent event, final Languages language) throws IOException {
     	final Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(frame.toString())), getLanguage(language));
@@ -25,11 +27,20 @@ public class PageLoaderImpl implements PageLoader{
         window.setScene(scene);
         window.show();
 	}
-    
+
+    @Override
+    public void goToSelectedPageOnInput(final Frames frame, final MouseEvent event, final Languages language, final int width, final int height) throws IOException {
+        final Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(frame.toString())), getLanguage(language));
+        final Scene scene = new Scene(root, width, height);
+        final Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
+        window.setScene(scene);
+        window.show();
+    }
+
     @Override
     public void goToSelectedPageOnInput(final Frames frame, final MouseEvent event) throws IOException {
         final Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(frame.toString())), getLanguage(Languages.ENGLISH));
-        final Scene scene = new Scene(root);
+        final Scene scene = new Scene(root, getScreenWidth(), getScreenHeight());
         final Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
         window.setScene(scene);
         window.show();
@@ -38,7 +49,7 @@ public class PageLoaderImpl implements PageLoader{
     @Override
     public void goToSelectedPageOnInput(final Frames frame) throws IOException { 
     	final Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(frame.toString())), getLanguage(Languages.ENGLISH));
-    	final Scene scene = new Scene(root);
+    	final Scene scene = new Scene(root, getScreenWidth(), getScreenHeight());
     	final Stage window = new Stage();
     	window.setScene(scene);
     	window.show();
@@ -52,11 +63,20 @@ public class PageLoaderImpl implements PageLoader{
         primaryStage.setScene(scene);
         primaryStage.show();
 	}
-    
+
+    @Override
+    public void loadFirstScene(final Stage primaryStage, final Languages language, final int width, final int height) throws IOException {
+        final Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(Frames.HOMEPAGE.toString())), getLanguage(language));
+        final Scene scene = new Scene(root, width, height);
+        primaryStage.setTitle("bullet-ballet");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     @Override
     public void loadFirstScene (final Stage primaryStage) throws IOException{
         final Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(Frames.HOMEPAGE.toString())), getLanguage(Languages.ENGLISH));
-        final Scene scene = new Scene(root);
+        final Scene scene = new Scene(root, getScreenWidth(), getScreenHeight());
         primaryStage.setTitle("bullet-ballet");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -71,7 +91,12 @@ public class PageLoaderImpl implements PageLoader{
     	return !Save.loadSettings().isEmpty() ? Save.loadSettings().get(Save.LANGUAGE_STRING) : language.getCountryCode();
     }
 
-	
+    private int getScreenWidth() {
+        return (!Save.loadSettings().isEmpty()) ? Integer.parseInt(Save.loadSettings().get(Save.RESOLUTION_WIDTH_STRING)) : Resolutions.getDefaultResolution().getWidth();
+    }
 
+    private int getScreenHeight() {
+        return (!Save.loadSettings().isEmpty()) ? Integer.parseInt(Save.loadSettings().get(Save.RESOLUTION_HEIGHT_STRING)) : Resolutions.getDefaultResolution().getHeight();
+    }
     
 }
