1edb1c939ef53c2ec5c476d7f69f7af7e6d0f504
Luca <luca.rengo@studio.unibo.it>
2022-02-19 15:54:15 +0100

    Update SoundImpl to retrieve the audio from the settings file.

--- src/main/java/it/unibo/pensilina14/bullet/ballet/sounds/SoundImpl.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/sounds/SoundImpl.java
@@ -1,12 +1,16 @@
 package it.unibo.pensilina14.bullet.ballet.sounds;
 
+import java.util.Map;
 import java.util.Optional;
 
+import it.unibo.pensilina14.bullet.ballet.save.Save;
 import javafx.scene.media.AudioClip;
 
 public class SoundImpl implements Sound{
 
-	private static final double VOLUME = 10;
+	private static final double NO_VOLUME = 0.0;
+
+	private static final double VOLUME = 10; //TODO: remove?
 	private final Optional<AudioClip> audioClip;
 	
 	public SoundImpl(final AudioClip audioClip) {
@@ -15,8 +19,11 @@ public class SoundImpl implements Sound{
 
 	@Override
 	public void play() {
-		//TODO recuperare salvataggio audio
-		this.audioClip.get().play(VOLUME);
+		// Cos' però fa 1 decriptazione, 1 lettura da file e 1 creazione di mappa con tutte le impostazioni ogni volta che viene chiamato play.
+		final Map<String, String> settingsMap = Save.loadSettings();
+		final double volume = !settingsMap.isEmpty() ? Double.parseDouble(settingsMap.get(Save.AUDIO_STRING)) : SoundImpl.NO_VOLUME;
+		this.audioClip.ifPresent(audioClip -> audioClip.play(volume));
+		//this.audioClip.get().play(volume); //TODO: remove
 	}
 
 	@Override
