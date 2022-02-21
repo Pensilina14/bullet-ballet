01d04c29c75d7051597a78314faaaf81a5130b5a
daze <leon.baiocchi@studio.unibo.it>
2022-02-21 19:20:14 +0100

    Refactor sprites classes: putting everything under PhysicalObjectSprite

--- src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/sprite/Images.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/graphics/sprite/Images.java
@@ -4,12 +4,16 @@ import it.unibo.pensilina14.bullet.ballet.graphics.map.Coins;
 
 public enum Images {
 
+    BULLET("Normal", "res/assets/sprites/bullets/Bullet.png"),
     POISONING_ITEM("poison", "res/assets/sprites/items/poison.png"),
     HEALING_ITEM("heart","res/assets/sprites/items/heart.png"),
     DAMAGING_ITEM("damage", "res/assets/sprites/items/snail.png"),
     BUNNY("bunny", "res/assets/sprites/obstacles/Jump.png"),
     WUT("dynamic obstacle", "res/assets/sprites/obstacles/dynamicObstacle.png"),
-	COIN("coin", Coins.getRandomCoinPath());
+	COIN("coin", Coins.getRandomCoinPath()),
+	GUN("Gun", "res/assets/sprites/weapons/gun.png"),
+	SHOTGUN("Shotgun", "res/assets/sprites/weapons/shotgun.png"),
+	AUTO("Auto", "res/assets/sprites/weapons/auto.png");
     
     private final String fileName;
     private final String objectName;
