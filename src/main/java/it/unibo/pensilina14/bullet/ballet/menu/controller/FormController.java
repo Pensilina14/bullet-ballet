8280d9a9e81c3aba59272e1682c37b766d453576
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2022-02-22 17:42:26 +0100

    add sound to form controller

--- src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/FormController.java
+++ src/main/java/it/unibo/pensilina14/bullet/ballet/menu/controller/FormController.java
@@ -5,6 +5,9 @@ import java.util.Objects;
 
 import it.unibo.pensilina14.bullet.ballet.Game;
 import it.unibo.pensilina14.bullet.ballet.graphics.scenes.AbstractScene;
+import it.unibo.pensilina14.bullet.ballet.sounds.Sounds;
+import it.unibo.pensilina14.bullet.ballet.sounds.SoundsFactory;
+import it.unibo.pensilina14.bullet.ballet.sounds.SoundsFactoryImpl;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
@@ -18,7 +21,7 @@ public class FormController {
 
 	@FXML
     private TextField insertionForm;
-
+	
 	@FXML
     void goBackOnMouseClicked(final MouseEvent event) throws IOException {
 		final PageLoader pageLoader = new PageLoaderImpl();
@@ -27,7 +30,6 @@ public class FormController {
 	
     @FXML
     void submitOnMouseClicked(final MouseEvent event) {
-    	System.out.println(this.insertionForm.getText());
     	if (this.insertionForm.getText().isBlank()) {
     		final Alert alert = new Alert(AlertType.ERROR
     				, "Blank form!");
@@ -38,6 +40,8 @@ public class FormController {
     }
     
     private void newGame(final MouseEvent event) {
+    	final SoundsFactory soundsFactory = new SoundsFactoryImpl();
+    	soundsFactory.createSound(Sounds.MENU_SOUND).play();
     	new AudioClip(Objects.requireNonNull(this.getClass().getResource("/menu_sound.mp4")).toExternalForm()).play();
     	final Game game = new Game(this.insertionForm.getText());
     	game.getSettings().setDifficulty(game.getSettings().getCurrentDifficulty());
